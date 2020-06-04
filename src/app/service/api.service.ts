50c7ad233c8bd91bc050daef9a71b5a244ff47ba
Gouda_Clark <mclarkuga@gmail.com>
2020-06-04 16:06:49 -0400

    deploy

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -9,7 +9,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 
 export class ApiService {
   httpClient = HttpClient
-  baseUri:string = 'http://localhost:4000/api';
+  baseUri:string = '/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient) { }
