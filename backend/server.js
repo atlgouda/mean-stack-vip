ec19350d56d957b8b7dfda2db70ca4f3c545869d
Gouda_Clark <mclarkuga@gmail.com>
2020-06-05 16:09:33 -0400

    check

--- backend/server.js
+++ backend/server.js
@@ -30,12 +30,10 @@ app.use(bodyParser.urlencoded({
 app.use(cors())
 app.use(express.static(path.join(__dirname, '../dist/mean-vip-list')));
 console.log(__dirname)
-// app.get('/*', function(req, res) {
-//    res.sendFile(path.join(__dirname, '../dist/mean-vip-list/index.html'));
-// })
 app.get('/*', function(req, res) {
-   res.sendFile(path.join(__dirname, 'database/db.js'));
+   res.sendFile(path.join(__dirname, '../dist/mean-vip-list/index.html'));
 })
+
 
 // app.use('/', express.static(path.join(__dirname, 'dist/mean-vip-list')));
 app.use('/api', vipRoute)
