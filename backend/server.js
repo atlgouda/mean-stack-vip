c64dec1145e6b9b97448745dc483764a76b5e3d2
Gouda_Clark <mclarkuga@gmail.com>
2020-06-04 23:53:42 -0400

    absolute path

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'dist/mean-vip-list')));
 app.use('/', express.static(path.join(__dirname, 'dist/mean-vip-list')));
 app.use('/api', vipRoute)
 app.get('*', function (req, res) {
-   res.sendFile(__dirname + '/index.html');
+   res.sendFile("../src/index.html");
 });
 
 // if (process.env.NODE_ENV === "production") {
