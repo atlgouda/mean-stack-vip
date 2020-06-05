ff44cd72080b9e5774913ca6473ca1e68d1e6ede
Gouda_Clark <mclarkuga@gmail.com>
2020-06-05 01:07:03 -0400

    app.get *

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors())
 app.use(express.static(path.join(__dirname, '../dist/mean-vip-list')));
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
    res.sendFile(path.join(__dirname + '../dist/mean-vip-list/index.html'));
 })
 
