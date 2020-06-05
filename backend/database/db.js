8344e1cbd8eac1358d769056ae09c741ba2e9a01
Gouda_Clark <mclarkuga@gmail.com>
2020-06-05 01:26:52 -0400

    linked db

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb://heroku_z5kh62p6:jkptud4be0oe0vblka0fu4vg8f@ds241875.mlab.com:41875/heroku_z5kh62p6'
+    db: 'mongodb://atlgouda:atlgouda123@ds241875.mlab.com:41875/heroku_z5kh62p6'
   };
