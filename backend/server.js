4dc92db4be25c1254d19725e560eee15c63af0cc
Gouda_Clark <mclarkuga@gmail.com>
2020-06-04 22:58:59 -0400

    sendfile path

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'dist/mean-vip-list')));
 app.use('/', express.static(path.join(__dirname, 'dist/mean-vip-list')));
 app.use('/api', vipRoute)
 app.get('*', function (req, res) {
-   res.sendFile('index.html', { root: __dirname });
+   res.sendFile('index.html', path.resolve(__dirname, '.../dist/mean-vip-list'));
  });
 
 // if (process.env.NODE_ENV === "production") {
